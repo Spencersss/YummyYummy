579125d809cf73796dd79c76544ac2140ab60175
Spencersss <13088535+Spencersss@users.noreply.github.com>
2020-12-28 18:33:53 -0500

    init repo

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from "./pages/main.js";
+import '../src/main.css';
+
+ReactDOM.render(<Main />, document.getElementById("root"));
---
+++ src/pages/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Yummy Yummy Kitchen</h1>
+                <p>paragraphh textttttttttt</p>
+            </div>
+        )
+    }
+
+}
+
+export default Main;
