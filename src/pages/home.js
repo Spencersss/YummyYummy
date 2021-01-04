f63d75152913cb323cf1a9594c116d977c01ea14
Spencersss <13088535+Spencersss@users.noreply.github.com>
2021-01-04 15:35:09 -0500

    Started home page sections/ styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { Container, Row } from 'react-bootstrap';
+import { Container, Row, Image, Col } from 'react-bootstrap';
+
+const sectionImages = [
+    {name: "Appetizers", src: "apps.jpg"},
+    {name: "Chicken", src: "chicken.jpg"},
+    {name: "Fried Rice", src: "fried_rice.jpg"},
+]
 
 class HomePage extends Component {
 
@@ -8,16 +14,31 @@ class HomePage extends Component {
             <div>
                 <Container id="pageContainer">
                     <Row>
-                        <h1>
-                            Welcome to our family owned and operated Chinese 
-                            takeout Restaurant located in Schenectady, NY.
-                        </h1>
-                        <p>
-                            Yummy Yummy offers authentic and delicious Chinese take for lunch and dinner.
-                            Celebrating its 30th year in business, it’s no wonder that Yummy Yummy has been a premier Chinese food destination and local favorite.
-                            The food is prepared daily using fresh ingredients and Chinese cooking style passed down through generations.
-                        </p>
+                        <Col md={6} sm={12} xs={12} xl={6}>
+                            <Image fluid src="guest.jpg" />
+                        </Col>
+                        <Col md={6} sm={12} xs={12} xl={6}> 
+                            <h1>
+                                Welcome to our family owned and operated Chinese 
+                                takeout Restaurant located in Schenectady, NY.
+                            </h1>
+                            <p>
+                                Yummy Yummy offers authentic and delicious Chinese take for lunch and dinner.
+                                Celebrating its 30th year in business, it’s no wonder that Yummy Yummy has been a premier Chinese food destination and local favorite.
+                                The food is prepared daily using fresh ingredients and Chinese cooking style passed down through generations.
+                            </p>
+                        </Col>
                     </Row>  
+                    <Row>
+                        {sectionImages.map(section => 
+                            <Col sm={12} xs={12} lg={4}>
+                                <Image id="sectionImage" fluid src={section.src} />
+                                <h3 id="sectionLabel">
+                                    {section.name}
+                                </h3>
+                            </Col>
+                        )}
+                    </Row>
                 </Container>
             </div>
         )
